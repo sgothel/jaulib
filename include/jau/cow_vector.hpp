ff4252f37017aa2bedf347745b6986c9a23b75fc
Sven Gothel <sgothel@jausoft.com>
2020-10-18 02:05:23 +0200

    cow_write: Add CoW vector implementation, exposing lock-free read operations using SC-DRF atomic synchronization.
    See API doc for details.
    
    First bring-up test w/ direct_bt using DEBUG leak and sanitizer successful.
    
    Further testing ongoing, hence patched might followup - or not.
    
    So far the goals has been reached:
    
    - utilizing jau::sc_atomic_critical to synchronize with read operations
    
    - utilizing a recursive write mutex to lock all write operations only
    
    - underlying store is a shared_ptr<vector<T>>,
      which gets replaced on write operations -> CoW
    
    - same shared_ptr<vector<T>> can be used as snapshot (get_snapshot())
      w/o additional costs, e.g for read operations or even write operations.
    
      Since write mutex is exposed, using set_store(snapshot) allows
      user to implement more complex mutations.
      This is beingn used by direct_bt at one point.
    
    - adding convenient push_back_unique(..) and erase_matching(..) methods,
      as well as a for_each_cow(..) template to iterate over the snapshot
      with consistently defined content.

--- include/jau/cow_vector.hpp
+++ include/jau/cow_vector.hpp
@@ -36,6 +36,8 @@
 #include <vector>
 #include <algorithm>
 
+// #include <ext/alloc_traits.h>
+
 #include <jau/debug.hpp>
 #include <jau/basic_types.hpp>
 #include <jau/ordered_atomic.hpp>
@@ -43,19 +45,429 @@
 
 namespace jau {
 
-/**
- * Implementation of a Copy-On-Write (COW) Vector,
- * exposing <i>lock-free</i> SC-DRF atomic synchronization.
- * <p>
- * </p>
- * See also:
- * <pre>
- * - Sequentially Consistent (SC) ordering or SC-DRF (data race free) <https://en.cppreference.com/w/cpp/atomic/memory_order#Sequentially-consistent_ordering>
- * - std::memory_order <https://en.cppreference.com/w/cpp/atomic/memory_order>
- * </pre>
- */
-template <typename T> class cow_vector : public std::vector<T> {
-};
+    /**
+     * Implementation of a Copy-On-Write (CoW) vector,
+     * exposing <i>lock-free</i> read operations using SC-DRF atomic synchronization.
+     * <p>
+     * The vector's store is owned using a reference to the data structure,
+     * allowing its replacement on Copy-On-Write (CoW).
+     * </p>
+     * <p>
+     * Writing to the store utilizes a mutex lock to avoid data races
+     * on the instances' write operations only, leaving read operations <i>lock-free</i>.<br>
+     * Write operations replace the store reference with a new instance using
+     * jau::sc_atomic_critical to synchronize with read operations.
+     * </p>
+     * <p>
+     * Reading from the store is <i>lock-free</i> and accesses the store reference using
+     * jau::sc_atomic_critical to synchronizing with write operations.
+     * </p>
+     * <p>
+     * Naturally iterators are not supported directly,
+     * otherwise we would need to copy the store to iterate through.<br>
+     * However, one can utilize iteration using the shared snapshot
+     * of the underlying vector store via jau::cow_vector::get_snapshot().
+     * </p>
+     * See also:
+     * <pre>
+     * - Sequentially Consistent (SC) ordering or SC-DRF (data race free) <https://en.cppreference.com/w/cpp/atomic/memory_order#Sequentially-consistent_ordering>
+     * - std::memory_order <https://en.cppreference.com/w/cpp/atomic/memory_order>
+     * </pre>
+     */
+    template <typename Value_type, typename Alloc_type = std::allocator<Value_type>> class cow_vector {
+        private:
+            typedef std::vector<Value_type> vector_t;
+            typedef std::shared_ptr<vector_t> vector_ref;
+
+            vector_ref store_ref;
+            sc_atomic_bool sync_atomic;
+            std::recursive_mutex mtx_write;
+
+        public:
+            // ctor
+
+            cow_vector() noexcept
+            : store_ref(new vector_t()), sync_atomic(false) {}
+
+            explicit cow_vector(const Alloc_type & a) noexcept
+            : store_ref(new vector_t(a)), sync_atomic(false) { }
+
+            explicit cow_vector(size_t n, const Alloc_type& a = Alloc_type())
+            : store_ref(new vector_t(n, a)), sync_atomic(false) { }
+
+            cow_vector(size_t n, const Value_type& value, const Alloc_type& a = Alloc_type())
+            : store_ref(new vector_t(n, value, a)), sync_atomic(false) { }
+
+            cow_vector(const cow_vector& x)
+            : sync_atomic(false) {
+                vector_ref x_store_ref;
+                {
+                    sc_atomic_critical sync_x( const_cast<cow_vector *>(&x)->sync_atomic );
+                    x_store_ref = x.store_ref;
+                }
+                store_ref = vector_ref( new vector_t(*x_store_ref) );
+            }
+
+            explicit cow_vector(const vector_t& x)
+            : store_ref(new vector_t(x)), sync_atomic(false) { }
+
+            cow_vector(cow_vector && x) noexcept {
+                // swap store_ref
+                store_ref = x.store_ref;
+                x.store_ref = nullptr;
+                // not really necessary
+                sync_atomic = x.sync_atomic;
+            }
+
+            ~cow_vector() noexcept { }
+
+            /**
+             * Returns this instances' recursive write mutex, allowing user to
+             * implement more complex mutable write operations.
+             *
+             * @see jau::cow_vector::get_snapshot()
+             * @see jau::cow_vector::set_store()
+             */
+            std::recursive_mutex & get_write_mutex() { return mtx_write; }
+
+            /**
+             * Special case facility allowing the user to replace the current store
+             * with the given value, potentially acquired via jau::cow_vector::get_snapshot()
+             * and mutated while holding the jau::cow_vector::get_write_mutex() lock.
+             *
+             * @param new_store_ref the user store to be set
+             * @see jau::cow_vector::get_write_mutex()
+             * @see jau::cow_vector::get_snapshot()
+             */
+            void set_store(std::shared_ptr<std::vector<Value_type>> & new_store_ref) noexcept {
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                sc_atomic_critical sync( const_cast<cow_vector *>(this)->sync_atomic );
+                store_ref = new_store_ref;
+            }
+
+            // read access
+
+            /**
+             * Returns the current snapshot of the underlying shared std::vector<T> reference.
+             * <p>
+             * Note that this snapshot will be outdated by the next (concurrent) write operation,
+             * i.e. the returned reference are still valid but do not represent the current content
+             * of this cow_vector instance.
+             * </p>
+             * <p>
+             * This read operation is <i>lock-free</i>.
+             * </p>
+             * @see jau::cow_vector::get_write_mutex()
+             * @see jau::cow_vector::set_store()
+             */
+            std::shared_ptr<std::vector<Value_type>> get_snapshot() const noexcept {
+                sc_atomic_critical sync( const_cast<cow_vector *>(this)->sync_atomic );
+                return store_ref;
+            }
+
+            /**
+             * Like std::vector<T>::size().
+             * <p>
+             * This read operation is <i>lock-free</i>.
+             * </p>
+             */
+            size_t size() const noexcept {
+                sc_atomic_critical sync( const_cast<cow_vector *>(this)->sync_atomic );
+                return store_ref->size();
+            }
+
+            /**
+             * Like std::vector<T>::operator[](size_t).
+             * <p>
+             * This read operation is <i>lock-free</i>.
+             * </p>
+             */
+            const Value_type & operator[](size_t i) const noexcept {
+                sc_atomic_critical sync( const_cast<cow_vector *>(this)->sync_atomic );
+                return (*store_ref)[i];
+            }
+
+            /**
+             * Like std::vector<T>::operator[](size_t).
+             * <p>
+             * This read operation is <i>lock-free</i>.
+             * </p>
+             */
+            Value_type & operator[](size_t i) noexcept {
+                sc_atomic_critical sync( const_cast<cow_vector *>(this)->sync_atomic );
+                return (*store_ref)[i];
+            }
+
+            /**
+             * Like std::vector<T>::at(size_t).
+             * <p>
+             * This read operation is <i>lock-free</i>.
+             * </p>
+             */
+            const Value_type & at(size_t i) const noexcept {
+                sc_atomic_critical sync( const_cast<cow_vector *>(this)->sync_atomic );
+                return store_ref->at(i);
+            }
+
+            /**
+             * Like std::vector<T>::at(size_t).
+             * <p>
+             * This read operation is <i>lock-free</i>.
+             * </p>
+             */
+            Value_type & at(size_t i) noexcept {
+                sc_atomic_critical sync( const_cast<cow_vector *>(this)->sync_atomic );
+                return store_ref->at(i);
+            }
+
+            // write access
+
+            /**
+             * Like std::vector<T>::operator=().
+             * <p>
+             * This write operation uses a mutex lock and is blocking this instances' write operations only.
+             * </p>
+             */
+            cow_vector& operator=(const cow_vector& x) {
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                vector_ref x_store_ref;
+                {
+                    sc_atomic_critical sync_x( const_cast<cow_vector *>(&x)->sync_atomic );
+                    x_store_ref = x.store_ref;
+                }
+                vector_ref new_store_ref = vector_ref( new vector_t(*x_store_ref) );
+                {
+                    sc_atomic_critical sync(sync_atomic);
+                    store_ref = new_store_ref;
+                }
+                return *this;
+            }
+
+#if 0 // ext/alloc_traits.h
+            /**
+             * Like std::vector<T>::operator=().
+             * <p>
+             * This write operation uses a mutex lock and is blocking this instances' write operations only.
+             * </p>
+             */
+            cow_vector& operator=(const cow_vector& x) noexcept(_Alloc_traits::_S_nothrow_move()) {
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                vector_ref x_store_ref;
+                {
+                    sc_atomic_critical sync_x( const_cast<cow_vector *>(&x)->sync_atomic );
+                    x_store_ref = x.store_ref;
+                }
+                vector_ref new_store_ref = vector_ref( new vector_t(*x_store_ref) );
+                {
+                    sc_atomic_critical sync(sync_atomic);
+                    store_ref = new_store_ref;
+                }
+                return *this;
+            }
+#endif
+
+            /**
+             * Like std::vector<T>::clear(), but ending with zero capacity.
+             * <p>
+             * This write operation uses a mutex lock and is blocking this instances' write operations.
+             * </p>
+             */
+            void clear() noexcept {
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                vector_ref new_store_ref = vector_ref( new vector_t() );
+                {
+                    sc_atomic_critical sync(sync_atomic);
+                    store_ref = new_store_ref;
+                }
+            }
+
+            /**
+             * Like std::vector<T>::swap().
+             * <p>
+             * This write operation uses a mutex lock and is blocking both cow_vector instance's write operations.
+             * </p>
+             */
+            void swap(cow_vector& x) noexcept {
+                std::unique_lock<std::recursive_mutex> lock(mtx_write, std::defer_lock); // utilize std::lock(a, b), allowing mixed order waiting on either object
+                std::unique_lock<std::recursive_mutex> lock_x(x.mtx_write, std::defer_lock); // otherwise RAII-style relinquish via destructor
+                std::lock(lock, lock_x);
+                {
+                    sc_atomic_critical sync_x( const_cast<cow_vector *>(&x)->sync_atomic );
+                    sc_atomic_critical sync(sync_atomic);
+                    vector_ref x_store_ref = x.store_ref;
+                    x.store_ref = store_ref;
+                    store_ref = x_store_ref;
+                }
+            }
+
+            /**
+             * Like std::vector<T>::pop_back().
+             * <p>
+             * This write operation uses a mutex lock and is blocking this instances' write operations only.
+             * </p>
+             */
+            void pop_back() noexcept {
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                vector_ref new_store_ref = vector_ref( new vector_t(*store_ref) );
+                new_store_ref->pop_back();
+                {
+                    sc_atomic_critical sync(sync_atomic);
+                    store_ref = new_store_ref;
+                }
+            }
+
+            /**
+             * Like std::vector<T>::push_back().
+             * <p>
+             * This write operation uses a mutex lock and is blocking this instances' write operations only.
+             * </p>
+             * @param x the value to be added at the tail.
+             */
+            void push_back(const Value_type& x) {
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                vector_ref new_store_ref = vector_ref( new vector_t(*store_ref) );
+                new_store_ref->push_back(x);
+                {
+                    sc_atomic_critical sync(sync_atomic);
+                    store_ref = new_store_ref;
+                }
+            }
+
+            /**
+             * Like std::vector<T>::push_back().
+             * <p>
+             * This write operation uses a mutex lock and is blocking this instances' write operations only.
+             * </p>
+             */
+            void push_back(Value_type&& x) {
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                vector_ref new_store_ref = vector_ref( new vector_t(*store_ref) );
+                new_store_ref->push_back(x);
+                {
+                    sc_atomic_critical sync(sync_atomic);
+                    store_ref = new_store_ref;
+                }
+            }
+
+            /**
+             * Generic Value_type equal comparator to be user defined for e.g. jau::cow_vector::push_back_unique().
+             * @param a one element of the equality test.
+             * @param b the other element of the equality test.
+             * @return true if both are equal
+             */
+            typedef bool(*equal_comparator)(const Value_type& a, const Value_type& b);
+
+            /**
+             * Like std::vector<T>::push_back(), but only if the newly added element does not yet exist.
+             * <p>
+             * This write operation uses a mutex lock and is blocking this instances' write operations only.
+             * </p>
+             * <p>
+             * Examples
+             * <pre>
+             *     static jau::cow_vector<Thing>::equal_comparator thingEqComparator =
+             *                  [](const Thing &a, const Thing &b) -> bool { return a == b; };
+             *     ...
+             *     jau::cow_vector<Thing> list;
+             *
+             *     bool added = list.push_back_unique(new_element, thingEqComparator);
+             *     ...
+             *     cow_vector<std::shared_ptr<Thing>> listOfRefs;
+             *     bool added = listOfRefs.push_back_unique(new_element,
+             *                    [](const std::shared_ptr<Thing> &a, const std::shared_ptr<Thing> &b) -> bool { return *a == *b; });
+             * </pre>
+             * </p>
+             * @param x the value to be added at the tail, if not existing yet.
+             * @param comparator the equal comparator to return true if both given elements are equal
+             * @return true if the element has been uniquely added, otherwise false
+             */
+            bool push_back_unique(const Value_type& x, equal_comparator comparator) {
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                for(auto it = store_ref->begin(); it != store_ref->end(); ) {
+                    if( comparator( *it, x ) ) {
+                        return false; // already included
+                    } else {
+                        ++it;
+                    }
+                }
+                vector_ref new_store_ref = vector_ref( new vector_t(*store_ref) );
+                new_store_ref->push_back(x);
+                {
+                    sc_atomic_critical sync(sync_atomic);
+                    store_ref = new_store_ref;
+                }
+                return true;
+            }
+
+            /**
+             * Erase either the first matching element or all matching elements.
+             * <p>
+             * This write operation uses a mutex lock and is blocking this instances' write operations only.
+             * </p>
+             * <p>
+             * Examples
+             * <pre>
+             *     cow_vector<Thing> list;
+             *     int count = list.erase_matching(element, true,
+             *                    [](const Thing &a, const Thing &b) -> bool { return a == b; });
+             *     ...
+             *     static jau::cow_vector<Thing>::equal_comparator thingRefEqComparator =
+             *                  [](const std::shared_ptr<Thing> &a, const std::shared_ptr<Thing> &b) -> bool { return *a == *b; };
+             *     ...
+             *     cow_vector<std::shared_ptr<Thing>> listOfRefs;
+             *     int count = listOfRefs.erase_matching(element, false, thingRefEqComparator);
+             * </pre>
+             * </p>
+             * @param x the value to be added at the tail, if not existing yet.
+             * @param all_matching if true, erase all matching elements, otherwise only the first matching element.
+             * @param comparator the equal comparator to return true if both given elements are equal
+             * @return number of erased elements
+             */
+            int erase_matching(const Value_type& x, const bool all_matching, equal_comparator comparator) {
+                int count = 0;
+                const std::lock_guard<std::recursive_mutex> lock(mtx_write);
+                vector_ref new_store_ref = vector_ref( new vector_t(*store_ref) );
+                for(auto it = new_store_ref->begin(); it != new_store_ref->end(); ) {
+                    if( comparator( *it, x ) ) {
+                        it = new_store_ref->erase(it);
+                        count++;
+                        if( !all_matching ) {
+                            break;
+                        }
+                    } else {
+                        ++it;
+                    }
+                }
+                if( 0 < count ) { // mutated new_store_ref?
+                    sc_atomic_critical sync(sync_atomic);
+                    store_ref = new_store_ref;
+                } // else throw away new_store_ref
+                return count;
+            }
+    };
+
+    /**
+     * Custom for_each template on a jau::cow_vector snapshot
+     * using jau::cow_vector::get_snapshot().
+     * <p>
+     * jau::for_each_idx() would also be usable, however,
+     * iterating through the snapshot preserves consistency over the
+     * fetched collection at the time of invocation.
+     * </p>
+     * <p>
+     * Method performs UnaryFunction on all snapshot elements [0..n-1].
+     * </p>
+     */
+    template<typename Value_type, class UnaryFunction>
+    constexpr UnaryFunction for_each_cow(cow_vector<Value_type> &cow, UnaryFunction f)
+    {
+        std::shared_ptr<std::vector<Value_type>> snapshot = cow.get_snapshot();
+        const size_t size = snapshot->size();
+        for (size_t i = 0; i < size; i++) {
+            f( (*snapshot)[i] );
+        }
+        return f; // implicit move since C++11
+    }
 
 } /* namespace jau */
 
