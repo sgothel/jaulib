7da584910b3f38201571d5817537b69558ec60e1
Sven Gothel <sgothel@jausoft.com>
2021-11-16 08:43:32 +0100

    latch: Move into namespace jau

--- include/jau/latch.hpp
+++ include/jau/latch.hpp
@@ -31,82 +31,86 @@
 
 #include <jau/ordered_atomic.hpp>
 
-/**
- * Inspired by std::latch of C++20
- *
- * @see https://en.cppreference.com/w/cpp/thread/latch
- */
-class latch {
-    private:
-        std::mutex mtx_cd;
-        std::mutex mtx_cv;
-        std::condition_variable cv;
-        jau::sc_atomic_size_t count;
+namespace jau {
 
-    public:
-        /** Returns the maximum value of the internal counter supported by the implementation. */
-        static constexpr size_t max() noexcept { return std::numeric_limits<size_t>::max(); }
+    /**
+     * Inspired by std::latch of C++20
+     *
+     * @see https://en.cppreference.com/w/cpp/thread/latch
+     */
+    class latch {
+        private:
+            std::mutex mtx_cd;
+            std::mutex mtx_cv;
+            std::condition_variable cv;
+            jau::sc_atomic_size_t count;
 
-        constexpr latch(const size_t count_) noexcept
-        : count(count_) {}
+        public:
+            /** Returns the maximum value of the internal counter supported by the implementation. */
+            static constexpr size_t max() noexcept { return std::numeric_limits<size_t>::max(); }
 
-        latch(const latch& o) = delete;
+            constexpr latch(const size_t count_) noexcept
+            : count(count_) {}
 
-        /**
-         * Atomically decrements the internal counter by n
-         * and notifies all blocked wait() threads if zero is reached.
-         *
-         * If n is greater than the value of the internal counter, the counter is set to zero.
-         *
-         * This operation strongly happens-before all calls that are unblocked on this latch.
-         *
-         * @param n the value by which the internal counter is decreased, defaults to 1
-         */
-        void count_down(const size_t n = 1) noexcept {
-            bool notify;
-            {
-                std::unique_lock<std::mutex> lock(mtx_cd); // Avoid data-race on concurrent count_down() calls
-                if( n < count ) {
-                    count -= n;
-                    notify = false;
-                } else {
-                    count = 0;
-                    notify = true;
+            latch(const latch& o) = delete;
+
+            /**
+             * Atomically decrements the internal counter by n
+             * and notifies all blocked wait() threads if zero is reached.
+             *
+             * If n is greater than the value of the internal counter, the counter is set to zero.
+             *
+             * This operation strongly happens-before all calls that are unblocked on this latch.
+             *
+             * @param n the value by which the internal counter is decreased, defaults to 1
+             */
+            void count_down(const size_t n = 1) noexcept {
+                bool notify;
+                {
+                    std::unique_lock<std::mutex> lock(mtx_cd); // Avoid data-race on concurrent count_down() calls
+                    if( n < count ) {
+                        count -= n;
+                        notify = false;
+                    } else {
+                        count = 0;
+                        notify = true;
+                    }
+                }
+                if( notify ) {
+                    cv.notify_all();
                 }
             }
-            if( notify ) {
-                cv.notify_all();
+
+            /**
+             * Returns true only if the internal counter has reached zero.
+             */
+            bool try_wait() const noexcept {
+                return 0 == count;
             }
-        }
 
-        /**
-         * Returns true only if the internal counter has reached zero.
-         */
-        bool try_wait() const noexcept {
-            return 0 == count;
-        }
-
-        /**
-         * Blocks the calling thread until the internal counter reaches 0. If it is zero already, returns immediately.
-         */
-        void wait() const noexcept {
-            if( 0 < count ) {
-                std::unique_lock<std::mutex> lock(mtx_cv);
-                cv.wait(lock, [&count]{ return 0 == count; });
+            /**
+             * Blocks the calling thread until the internal counter reaches 0. If it is zero already, returns immediately.
+             */
+            void wait() const noexcept {
+                if( 0 < count ) {
+                    std::unique_lock<std::mutex> lock(mtx_cv);
+                    cv.wait(lock, [&count]{ return 0 == count; });
+                }
             }
-        }
 
-        /**
-         * Atomically decrements the internal counter by n and (if necessary) blocks the calling thread until the counter reaches zero.
-         *
-         * Equivalent to count_down(n); wait();.
-         *
-         * @param n the value by which the internal counter is decreased, defaults to 1
-         */
-        void arrive_and_wait(const size_t n = 1) noexcept {
-            count_down(n);
-            wait();
-        }
-};
+            /**
+             * Atomically decrements the internal counter by n and (if necessary) blocks the calling thread until the counter reaches zero.
+             *
+             * Equivalent to count_down(n); wait();.
+             *
+             * @param n the value by which the internal counter is decreased, defaults to 1
+             */
+            void arrive_and_wait(const size_t n = 1) noexcept {
+                count_down(n);
+                wait();
+            }
+    };
+
+} /* namespace jau */
 
 #endif /* JAU_LATCH_HPP_ */
