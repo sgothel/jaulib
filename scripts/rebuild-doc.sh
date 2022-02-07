9378bc023b5004e023194bb27d3e721b621e93fd
Sven Gothel <sgothel@jausoft.com>
2022-02-07 14:46:39 +0100

    *build-doc.sh: Only build documentation and remove tar ball before creating (safe)

--- scripts/rebuild-doc.sh
+++ scripts/rebuild-doc.sh
@@ -16,37 +16,29 @@ fi
 
 CPU_COUNT=`getconf _NPROCESSORS_ONLN`
 
-# run as root 'dpkg-reconfigure locales' enable 'en_US.UTF-8'
-# perhaps run as root 'update-locale LC_MEASUREMENT=en_US.UTF-8 LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8'
-export LC_MEASUREMENT=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+# run 'dpkg-reconfigure locales' enable 'en_US.UTF-8'
 export LANG=en_US.UTF-8
+export LC_MEASUREMENT=en_US.UTF-8
 
 buildit() {
     echo rootdir $rootdir
     echo logfile $logfile
     echo CPU_COUNT $CPU_COUNT
 
-    cd $rootdir
-    rm -rf dist-$archabi
-    mkdir -p dist-$archabi/bin
-    rm -rf build-$archabi
-    mkdir -p build-$archabi
-    cd build-$archabi
-    # CLANG_ARGS="-DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++"
-    cmake $CLANG_ARGS -DCMAKE_INSTALL_PREFIX=$rootdir/dist-$archabi -DBUILDJAVA=ON -DBUILD_TESTING=ON ..
+    cd $rootdir/build-$archabi
     make -j $CPU_COUNT doc_jau
     if [ $? -eq 0 ] ; then
-        echo "BUILD SUCCESS $bname $archabi"
+        echo "REBUILD SUCCESS $bname $archabi"
         rm -f $rootdir/documentation.tar.xz
         tar caf $rootdir/documentation.tar.xz documentation
         cd $rootdir
         return 0
     else
-        echo "BUILD FAILURE $bname $archabi"
+        echo "REBUILD FAILURE $bname $archabi"
         cd $rootdir
         return 1
     fi
 }
 
 buildit 2>&1 | tee $logfile
+
