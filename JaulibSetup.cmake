cf80b660bee90f9cb15c5b252528f67fca82d631
Sven Göthel <sgothel@jausoft.com>
2024-05-26 07:26:18 +0200

    JaulibSetup: Integrate wasm/emscripten setup

--- JaulibSetup.cmake
+++ JaulibSetup.cmake
@@ -322,6 +322,30 @@ if(NOT EMSCRIPTEN)
       /usr/include
     )
   endif()
+else()
+    message(STATUS "JaulibSetup: ${PROJECT_NAME} (wasm) EMSCRIPTEN")
+    # See https://emscripten.org/docs/tools_reference/settings_reference.html
+    #
+    # set(EMS_FLAGS "--use-port=sdl2 --use-port=sdl2_image --use-port=sdl2_ttf")
+    set(EMS_FLAGS "-s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_TTF=2 -Wno-unused-command-line-argument")
+    set(EMS_FLAGS "${EMS_FLAGS} -s WASM=1 -s LZ4=1 -s EXPORTED_RUNTIME_METHODS=cwrap")
+    # set(EMS_FLAGS "${EMS_FLAGS} -s FULL_ES2=1") # would use client-side memory like FULL_ES3 -> bad performance
+    set(EMS_FLAGS "${EMS_FLAGS} -s MAX_WEBGL_VERSION=2") # WebGL 2 -> ES3
+    set(EMS_FLAGS "${EMS_FLAGS} -s ALLOW_MEMORY_GROWTH=1")
+    # set(EMS_FLAGS "${EMS_FLAGS} -pthread") # fights w/ ALLOW_MEMORY_GROWTH
+    # set(EMS_FLAGS "${EMS_FLAGS} -s MEMORY64=1") # wasm64 end-to-end: wasm32 object file can't be linked in wasm64 mode
+    # set(EMS_FLAGS "${EMS_FLAGS} -s ASSERTIONS=1")
+    set(EMS_FLAGS "${EMS_FLAGS} -s STACK_OVERFLOW_CHECK=1") # cheap cockie magic, enables CHECK_NULL_WRITES
+    # set(EMS_EXE_FLAGS "-s SIDE_MODULE=1")
+    set(EMS_EXE_FLAGS "")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${EMS_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${EMS_FLAGS}")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${EMS_FLAGS}")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${EMS_FLAGS}")
+    set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS}")
+    # set(DONT_USE_RTTI ON)
+    set(USE_LIBUNWIND OFF)
+    message(STATUS "JaulibSetup: EMS_FLAGS = ${EMS_FLAGS}")
 endif()
 
 if(${OS_NAME} STREQUAL "linux")
@@ -335,6 +359,8 @@ endif()
 message(STATUS "JaulibSetup: CMAKE_CXX_FLAGS = ${CMAKE_CXX_FLAGS}")
 message(STATUS "JaulibSetup: CMAKE_SHARED_LINKER_FLAGS = ${CMAKE_SHARED_LINKER_FLAGS}")
 message(STATUS "JaulibSetup: CMAKE_EXE_LINKER_FLAGS = ${CMAKE_EXE_LINKER_FLAGS}")
+message(STATUS "JaulibSetup: CMAKE_SHARED_LINKER_FLAGS = ${CMAKE_SHARED_LINKER_FLAGS}")
+message(STATUS "JaulibSetup: CMAKE_STATIC_LINKER_FLAGS = ${CMAKE_STATIC_LINKER_FLAGS}")
 message(STATUS "JaulibSetup: CMAKE_CXX_STANDARD_LIBRARIES = ${CMAKE_CXX_STANDARD_LIBRARIES}")
 
 # Set CMAKE_INSTALL_XXXDIR (XXX {BIN LIB ..} if not defined
