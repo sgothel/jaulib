72653cc51e92e6b80edff8ede6fd14b7cc313f13
Sven Göthel <sgothel@jausoft.com>
2026-05-20 17:09:05 +0200

    scripts/setup_clang_alternatives.sh: Add git-clang-format

--- scripts/setup_clang_alternatives.sh
+++ scripts/setup_clang_alternatives.sh
@@ -7,10 +7,12 @@ update-alternatives --remove-all clangd
 update-alternatives --remove-all clang++
 update-alternatives --remove-all clang-cpp
 update-alternatives --remove-all clang-format
+update-alternatives --remove-all git-clang-format
 update-alternatives --remove-all clang-tidy
 update-alternatives --install /usr/bin/clang        clang        /usr/bin/clang-${CLANG_VERSION} 20
 update-alternatives --install /usr/bin/clangd       clangd       /usr/bin/clangd-${CLANG_VERSION} 20
 update-alternatives --install /usr/bin/clang++      clang++      /usr/bin/clang++-${CLANG_VERSION} 20
 update-alternatives --install /usr/bin/clang-cpp    clang-cpp    /usr/bin/clang-cpp-${CLANG_VERSION} 20
 update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-${CLANG_VERSION} 20
+update-alternatives --install /usr/bin/git-clang-format git-clang-format /usr/bin/git-clang-format-${CLANG_VERSION} 20
 update-alternatives --install /usr/bin/clang-tidy   clang-tidy   /usr/bin/clang-tidy-${CLANG_VERSION} 20
